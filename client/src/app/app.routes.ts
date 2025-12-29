2c0839b7c217c7ba3acab57b71a61e7037a5a7f9
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2025-12-30 01:54:21 +0530

    feat: add chat component and auth guard with routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { loginGuard } from './guards/login.guard';
+import { ChatComponent } from './chat/chat.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -11,5 +13,10 @@ export const routes: Routes = [
         path:'login',
         loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
         canActivate: [loginGuard]
-    }
+    },
+    {
+        path:'chat',
+        component: ChatComponent,
+        canActivate: [authGuard]
+    },
 ];
