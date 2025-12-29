564016aa983f61ff8fde60fca4d2262f2cb2f3a4
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2025-12-30 01:13:24 +0530

    feat: implement login functionality with error handling and success notification

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,7 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { AuthServiceService } from '../services/auth-service.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ApiResponse } from '../Models/api-response';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,14 +19,30 @@ export class LoginComponent {
   email!: string;
   password!: string;
 
+  private authService = inject(AuthServiceService);
+  private snakeBar = inject(MatSnackBar);
+  private router = inject(Router);
+
   hide = signal(false);
 
   togglePassword(event:MouseEvent) {
     this.hide.set(!this.hide());
+    event.stopPropagation();
   }
 
   login() {
-    // Implement login logic here
+    this.authService.login({email: this.email, password: this.password}).subscribe({
+      next: () => {
+        this.snakeBar.open('Login Successful', 'Close', {duration: 3000});
+      },
+      error: (error:HttpErrorResponse) => {
+        let err= error.error as ApiResponse<string>;
+        this.snakeBar.open(`Registration failed: ${err.error}`, 'Close', {duration: 3000});
+      },
+      complete: () => {
+        this.router.navigate(['/']);
+      }
+    });
   }
 
 }
--- client/src/app/services/auth-service.service.ts
+++ client/src/app/services/auth-service.service.ts
@@ -9,13 +9,24 @@ import { Observable, tap } from 'rxjs';
 export class AuthServiceService {
 
   private baseUrl = 'http://localhost:5001/api/account';
+  private token = "token"
 
   private httpClient = inject(HttpClient);
 
   register(data:FormData):Observable<ApiResponse<string>>{
     return this.httpClient.post<ApiResponse<string>>(`${this.baseUrl}/register`, data)
     .pipe(tap(response => {
-      localStorage.setItem('token', response.data);
+      localStorage.setItem(this.token, response.data);
+    }));
+  }
+
+  login(data:{email:string, password:string}):Observable<ApiResponse<string>>{
+    return this.httpClient.post<ApiResponse<string>>(`${this.baseUrl}/login`, data)
+    .pipe(tap(response => {
+      if (response.isSuccess){
+        localStorage.setItem(this.token, response.data);
+      }
+      return response;
     }));
   }
 
