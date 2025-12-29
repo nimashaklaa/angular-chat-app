74cca9cf4bb4738f716467dc13ccf5f8057bcfa3
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2025-12-30 00:06:32 +0530

    feat: enhance registration form with Angular Material components and add HTTP client support
    - Updated app.config.ts to include provideHttpClient for HTTP requests.
    - Redesigned register.component.html to implement a responsive registration form using Angular Material components.
    - Modified register.component.ts to include necessary Angular Material modules and set a default profile picture.
    - Added register.svg for visual enhancement of the registration page.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,9 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { AuthServiceService } from '../services/auth-service.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatFormFieldModule, FormsModule, MatInputModule, MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -11,7 +15,7 @@ export class RegisterComponent {
   email!: string;
   password!: string;
   fullName!: string;
-  profilePicture!: string;
+  profilePicture: string = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y';
   profileImage: File |null = null;
 
   authService = inject(AuthServiceService);
