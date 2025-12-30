81b959426efd4fbdf84134a8a128962255b4f3f5
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2025-12-30 06:09:00 +0530

    feat: implement chat layout with sidebar and window components

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ChatSidebarComponent } from '../components/chat-sidebar/chat-sidebar.component';
+import { ChatWindowComponent } from '../components/chat-window/chat-window.component';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [ChatSidebarComponent, ChatWindowComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
